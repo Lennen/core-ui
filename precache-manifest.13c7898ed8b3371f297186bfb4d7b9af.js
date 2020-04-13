b2ccb2fd09bbfa8598d24d3fc60bfa2e063c096f
Travis-CI <comindware-awesome-b@comindware.com>
2020-04-13 20:32:54 +0000

    Auto-deploy to Github Pages

---
+++ precache-manifest.13c7898ed8b3371f297186bfb4d7b9af.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "url": "icon_512x512.fcff4271d5ae3728a9de51fcd51f1bc8.png"
+  },
+  {
+    "url": "icon_72x72.120a434576809a7246b0e3370eef5f3f.png"
+  },
+  {
+    "revision": "8086e197694282b26c7b729945ecc377",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2"
+  },
+  {
+    "revision": "52b8345b6d8e11988d8db917c366b4da",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff"
+  },
+  {
+    "revision": "f72d546abcc27880adf8428792c72519",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.eot"
+  },
+  {
+    "revision": "1709a2810c3752483683f3f7d287aa89",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf"
+  },
+  {
+    "revision": "27603739fe78fee587c7b84873475b61",
+    "url": "node_modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.svg"
+  },
+  {
+    "revision": "656482e5b93a850197ffd5a25ddaf9ed",
+    "url": "index.html"
+  },
+  {
+    "url": "icon_96x96.7aff45e523c76f5d83459fe6ebd25fe6.png"
+  },
+  {
+    "url": "icon_384x384.7b318db198bec11c4586efb16efe2928.png"
+  },
+  {
+    "url": "icon_192x192.3420fe48c7b98b3e48be6ef2d8058cd0.png"
+  },
+  {
+    "url": "icon_152x152.b2666e72872ff41915d0a403f7d64ecf.png"
+  },
+  {
+    "url": "icon_144x144.1a171c39c07bf6b29d652e00a46adc2d.png"
+  },
+  {
+    "url": "icon_128x128.714d63aeda32d131b01d0ef137fd68a1.png"
+  },
+  {
+    "revision": "0860f5b97a9939c6db36",
+    "url": "app.js"
+  },
+  {
+    "revision": "0860f5b97a9939c6db36",
+    "url": "app.css"
+  }
+];
